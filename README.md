3d351217aa5874d9297406072596d1e50e2db717
ashrafbd1496 <asraf1496@gmail.com>
2023-12-23 08:42:36 +0600

    class 4 done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# vue-beginner
+[Playlist] - (https://www.youtube.com/watch?v=CS4LAhAogpU&list=PLZ8kLhUbDAhADR0nUr2rwhOD0smxVZX-x)
+
+[Mamunur Github](https://www.youtube.com/watch?v=CS4LAhAogpU&list=PLZ8kLhUbDAhADR0nUr2rwhOD0smxVZX-x)
--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const { createApp, ref } = Vue;
 
 createApp({
   setup() {
-    const msg = ref("Hello vue!");
+    const myInfo = ref({
+      name: "Ashrf Uddin",
+      gituhubLink: "https://github.com/ashrafbd1496/vue-beginner",
+      rawHmtl: '<a href="https://github.com/ashrafbd1496/vue-beginner"></a>',
+    });
+
     return {
-      msg,
+      myInfo,
     };
   },
 }).mount("#app");
