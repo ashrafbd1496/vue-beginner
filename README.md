c3e2a4aa7f444645f993a7bc8fdc3ad7269db725
ashrafbd1496 <asraf1496@gmail.com>
2023-12-25 11:48:19 +0600

    Shopping list project done

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [Mamunur Github](https://www.youtube.com/watch?v=CS4LAhAogpU&list=PLZ8kLhUbDAhADR0nUr2rwhOD0smxVZX-x)
 
 [vue school repo](https://github.com/vueschool/vuejs-3-fundamentals/blob/4f4aa9e67b986981efb803a8e6d67c06fbd1b5e6/main.css)
+
+[vue js laracast](https://www.youtube.com/watch?v=BZwn47RPiAM&list=PL3VM-unCzF8jX-GoazLPcbi7M0wJux8F-)
+
+[vue js udemy](https://www.udemy.com/course/learn-vuejs-by-making-applications-bangla-tutorial/learn/lecture/9150842#overview)
--- shopping-list/main.js
+++ shopping-list/main.js
@@ -1,28 +1,43 @@
-const { createApp, ref } = Vue;
+const { createApp, ref, computed } = Vue; // Removed unused watchEffect
 
 const app = createApp({
   setup() {
     const heading = ref("Shopping List");
-    const items = [
+    const items = ref([
       { name: "Meat & Fish", price: 500.0 },
       { name: "Oil & Fat", price: 200.0 },
       { name: "Dairy & Eggs", price: 120.0 },
-    ];
+    ]);
+
     const newItem = ref("");
+
+    const total = computed(() => {
+      return items.value.reduce((acc, item) => acc + item.price, 0);
+    });
 
     return {
       heading,
       items,
       newItem,
+      total,
+
       addItem() {
-        items.push({
+        items.value.push({
           name: newItem.value,
           price: 0.0,
         });
         newItem.value = "";
       },
-    }; //end return
-  }, //end Setup
-}); //end create app
+      removeItem(index) {
+        this.items.splice(index, 1);
+      },
+
+      updatePrice(index, newPrice) {
+        // Add a method to update item prices
+        items.value[index].price = newPrice;
+      },
+    };
+  },
+});
 
 app.mount("#app");
